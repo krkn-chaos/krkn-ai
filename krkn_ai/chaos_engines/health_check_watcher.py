12ddbffe128236e3f6458564d195f2c181d7505d
Mrinal Chaturvedi <mrinal.chaturvedi27@gmail.com>
2026-05-08 14:41:06 +0530

    Skip sparse
    Signed-off-by: Mrinal Chaturvedi <mrinal.chaturvedi27@gmail.com>

--- krkn_ai/chaos_engines/health_check_watcher.py
+++ krkn_ai/chaos_engines/health_check_watcher.py
@@ -128,7 +128,7 @@ class HealthCheckWatcher:
                     response_times.append(result.response_time)
 
             if len(response_times) < 4:  # Not enough data to calculate outliers
-                return 0
+                continue
 
             q1 = np.percentile(response_times, 25)
             q3 = np.percentile(response_times, 75)
